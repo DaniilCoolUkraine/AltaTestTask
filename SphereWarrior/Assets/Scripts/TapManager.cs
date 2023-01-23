43c5c511b9aec7ac0c8898417fdf9f08ed476ddd
DaniilCoolUkraine <cd0games2000@gmail.com>
2023-01-23 20:28:11 +0200

    Added touch controls

---
+++ SphereWarrior/Assets/Scripts/TapManager.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using UnityEngine;
+
+namespace SphereWarrior
+{
+    public class TapManager : MonoBehaviour
+    {
+        [SerializeField] private float _sizePerFrame;
+        
+        public static event Action OnTap;
+        public static event Action OnTapReleased;
+        public static event Action<float> OnTapHold;
+
+        private void Update()
+        {
+            if (Input.touchCount > 0)
+            {
+                Touch touch = Input.GetTouch(0);
+                
+                if (touch.phase == TouchPhase.Began)
+                    OnTap?.Invoke();
+                
+                if (touch.phase == TouchPhase.Ended)
+                    OnTapReleased?.Invoke();
+
+                if (touch.phase == TouchPhase.Stationary)
+                    OnTapHold?.Invoke(_sizePerFrame);
+            }
+        }
+    }
+}
